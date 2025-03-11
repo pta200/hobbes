902d4cac929551d8d3281f7fe421548325855372
pta200 <pta200@gmail.com>
2025-03-11 09:26:17 -0400

    Update run_service.sh

--- run_service.sh
+++ run_service.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-poetry run uvicorn hobbes.main:app --host 0.0.0.0 --port 8000
+# start specific fluxIAM process depending on parameter 
+if [ -z "$1" ]
+  then
+    echo "No argument supplied: service|worker|dashboard"
+    exit 0
+fi
+
+case $1 in 
+    service) 
+      poetry run uvicorn hobbes.main:app --host 0.0.0.0 --port 8000
+    worker)  
+      celery -A hobbes.worker.celery_app worker $2
+      ;;
+    dashboard)  
+      celery flower --port=5555 $2
+      ;;
+    *)
+    echo "unknown"
+    exit 0
+    ;;
+esac
