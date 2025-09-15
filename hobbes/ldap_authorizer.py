0412beac29eddde6aacb260805c09d5252523422
Paolo Audiberti <pta200@gmail.com>
2025-09-15 16:48:57 -0400

    add custom flower template for flower login

--- hobbes/ldap_authorizer.py
+++ hobbes/ldap_authorizer.py
@@ -6,6 +6,7 @@ import logging
 from urllib.parse import urlencode
 
 import tornado.auth
+import os
 import tornado.gen
 import tornado.web
 
@@ -16,17 +17,34 @@ from flower.views import BaseHandler
 
 logger = logging.getLogger(__name__)
 
+
+
 class LDAPHandler(BaseHandler):
 
-    async def get_current_user(self):
+    def get_current_user(self):
         return self.get_secure_cookie('user')
 
-    async def get(self):
-        logger.info("GOING TO PAGE")
-        self.write('<html><body><form action="/login" method="post">'
-                   'Name: <input type="text" name="name">'
-                   '<input type="submit" value="Sign in">'
-                   '</form></body></html>')
+    def get_template_path(self):
+        # over default handler to load custom login template
+        return os.path.join(os.path.dirname(__file__), "flower_templates")
+
+
+    def get(self):
+        if self.get_secure_cookie('user'):
+            next_ = self.get_argument('next', self.application.options.url_prefix or '/')
+            if self.application.options.url_prefix and next_[0] != '/':
+                next_ = '/' + next_
+            self.redirect(next_)
+
+        else:
+            # self.write('<html><body><form action="/login" method="post">'
+            #            'Name: <input type="text" name="name">'
+            #            '<input type="submit" value="Sign in">'
+            #            '</form></body></html>')
+
+            message=''
+
+            self.render('login.html', message=message)
 
     def post(self):
         logger.info("SET USER......")
