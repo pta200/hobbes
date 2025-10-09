f8c3061dd2511d920a810c1d5424cd06ad69dedb
pta200 <pta200@gmail.com>
2025-10-09 09:41:40 -0400

    Update ldap_authorizer.py
    add ldap integration

--- hobbes/ldap_authorizer.py
+++ hobbes/ldap_authorizer.py
@@ -12,14 +12,41 @@ import tornado.web
 
 import tornado
 from tornado.options import options
+from ldap3 import Server, Connection, ALL
 
 from flower.views import BaseHandler
 
 logger = logging.getLogger(__name__)
 
-
+DOMAIN = os.getenv("AUTH_DOMAIN", "")
+ldap_uri = os.getenv("LDAP_SERVER_URI", "")
 
 class LDAPHandler(BaseHandler):
+
+    def _authenticate(self, username: str, password: str):
+        """
+        do simple bind on ldap for AD expect the username in UPN format e.g. user@yourdomain.com
+        """
+        try:
+            server = Server(ldap_uri, connect_timeout=5, get_info=ALL)
+            conn = Connection(server, 
+                user=user_name,
+                password=password,
+                auto_bind=True,
+                raise_exceptions=True, # raises exception on connection failure
+                receive_timeout=receive_timeout,
+                client_strategy=ldap3.SAFE_SYNC,
+            )
+            return True
+
+        except Exception as error:
+            logger.error(error)
+            return False
+            
+        finally:
+            if conn:
+                conn.unbind()
+                
 
     def get_current_user(self):
         return self.get_secure_cookie('user')
@@ -37,21 +64,24 @@ class LDAPHandler(BaseHandler):
             self.redirect(next_)
 
         else:
-            # self.write('<html><body><form action="/login" method="post">'
-            #            'Name: <input type="text" name="name">'
-            #            '<input type="submit" value="Sign in">'
-            #            '</form></body></html>')
-
-            message=''
-
-            self.render('login.html', message=message)
+            self.render('login.html', message="")
 
     def post(self):
-        logger.info("SET USER......")
-        cookie = f"{self.get_argument('name')}@gmail.com"
-        logger.info(cookie)
-        self.set_secure_cookie("user", cookie)
-        next_ = self.get_argument('next', self.application.options.url_prefix or '/')
-        if self.application.options.url_prefix and next_[0] != '/':
-            next_ = '/' + next_
-        self.redirect(next_)
+        """
+        Handle Tornado post request from login page. Check for crendentials, authenticate
+        redirect accordingly
+        """
+        if not self.get_argument('username') or not self.get_argument('password'):
+            self.render('login.html', message="missing credentials")
+
+        else:
+            username = f"{self.get_argument('username')}@{DOMAIN}"
+            if self._authenticate(username, self.get_argument('password')):
+                self.set_secure_cookie("user", username)
+                next_ = self.get_argument('next', self.application.options.url_prefix or '/')
+                if self.application.options.url_prefix and next_[0] != '/':
+                    next_ = '/' + next_
+                self.redirect(next_)
+            else:
+                self.render('login.html', message="invalid credentials")
+        
