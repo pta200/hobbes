fca2aa529fe2dfd13fcac985d33653d2ca7742f2
pta200 <pta200@gmail.com>
2026-01-13 09:37:07 -0500

    Create conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,59 @@
+import asyncio
+import tempfile
+from asyncio import current_task
+
+import pytest
+import pytest_asyncio
+from fastapi.testclient import TestClient
+from sqlalchemy.ext.asyncio import (
+    async_scoped_session,
+    async_sessionmaker,
+    create_async_engine,
+)
+from sqlalchemy.pool import NullPool
+from sqlmodel import SQLModel
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+from hobbes.models import *
+from hobbes.main import app
+from hobbes.db_manager import get_async_session
+
+tmp_dir = tempfile.TemporaryDirectory()
+
+SQLALCHEMY_DATABASE_URL = f"sqlite+aiosqlite:///{tmp_dir.name}/hobbes.db"
+
+engine = create_async_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    poolclass=NullPool,
+)
+
+TestingSessionLocal = async_scoped_session(
+    async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession),
+    scopefunc=current_task,
+)
+
+
+async def init_db():
+    """create all tables"""
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+
+async def override_get_async_session():
+    """session override function"""
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        await db.close()
+
+
+app.dependency_overrides[get_async_session] = override_get_async_session
+asyncio.run(init_db())
+
+
+@pytest_asyncio.fixture
+async def client():
+    # init test client with app without entering lifespan context
+    return TestClient(app)
