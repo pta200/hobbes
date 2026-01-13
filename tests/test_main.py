41e6b41c6d7e7eb94e8bec6eaccaabf5f8aee93f
pta200 <pta200@gmail.com>
2026-01-13 09:38:36 -0500

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -15,47 +15,46 @@ from hobbes.models import *
 from hobbes.main import app
 from hobbes.db_manager import get_async_session
 
-tmp_dir = tempfile.TemporaryDirectory()
+# tmp_dir = tempfile.TemporaryDirectory()
 
-SQLALCHEMY_DATABASE_URL = f"sqlite+aiosqlite:///{tmp_dir.name}/hobbes.db"
+# SQLALCHEMY_DATABASE_URL = f"sqlite+aiosqlite:///{tmp_dir.name}/hobbes.db"
 
-engine = create_async_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False},
-    poolclass=StaticPool,
-)
-TestingSessionLocal = async_scoped_session(
-    async_sessionmaker(
-        engine,
-        expire_on_commit=False,
-    ),
-    scopefunc=current_task,
-)
-
-
-async def init_db():
-    """create all tables"""
-    async with engine.begin() as conn:
-        await conn.run_sync(SQLModel.metadata.create_all)
+# engine = create_async_engine(
+#     SQLALCHEMY_DATABASE_URL,
+#     connect_args={"check_same_thread": False},
+#     poolclass=StaticPool,
+# )
+# TestingSessionLocal = async_scoped_session(
+#     async_sessionmaker(
+#         engine,
+#         expire_on_commit=False,
+#     ),
+#     scopefunc=current_task,
+# )
 
 
-async def override_get_session():
-    """session overide function"""
-    try:
-        db = TestingSessionLocal()
-        yield db
-    finally:
-        await db.close()
+# async def init_db():
+#     """create all tables"""
+#     async with engine.begin() as conn:
+#         await conn.run_sync(SQLModel.metadata.create_all)
 
 
-app.dependency_overrides[get_async_session] = override_get_session
+# async def override_get_session():
+#     """session overide function"""
+#     try:
+#         db = TestingSessionLocal()
+#         yield db
+#     finally:
+#         await db.close()
 
-client = TestClient(app)
+
+# app.dependency_overrides[get_async_session] = override_get_session
+
+# client = TestClient(app)
 
 
 @pytest.mark.asyncio
-async def test_create_book():
-    await init_db()
+async def test_create_book(client):
     data = {
         "title": "Count of Monte Cristo",
         "isbn": f"{uuid.uuid4()}",
@@ -70,16 +69,13 @@ async def test_create_book():
 
 
 @pytest.mark.asyncio
-async def test_get_books():
-    await init_db()
+async def test_get_books(client):
     response = client.get("/v1/books/all")
     assert response.status_code == 200
 
 
 @pytest.mark.asyncio
-async def test_get_book_by_date():
-    await init_db()
-
+async def test_get_book_by_date(client):
     data = {
         "title": "Count of Monte Cristo",
         "isbn": f"{uuid.uuid4()}",
@@ -98,9 +94,7 @@ async def test_get_book_by_date():
 
 
 @pytest.mark.asyncio
-async def test_search_book():
-    await init_db()
-
+async def test_search_book(client):
     data = {
         "title": "Count of Monte Cristo",
         "isbn": f"{uuid.uuid4()}",
